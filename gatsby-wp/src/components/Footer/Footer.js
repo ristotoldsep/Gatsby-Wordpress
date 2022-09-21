17f0b1dd8c4de4b1656641e5e1406c4973a28bb8
Risto Tõldsep <r.toldsep@gmail.com>
2022-09-21 13:39:25 +0300

    Added overlay mobile menu component

---
+++ gatsby-wp/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Wrapper } from "./Footer.styles"
+
+const Footer = () => (
+    <Wrapper>
+        <p>
+            Do you want to get updates and news from Cake it? Contact us on the address below.
+        </p>
+        <p>
+            CakeIt! | Siduri 3 | @ristotoldsep
+        </p>
+    </Wrapper>
+)
+
+export default Footer
--- gatsby-wp/src/components/Layout/Layout.js
+++ gatsby-wp/src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Header from "../Header/Header"
 import Hamburger from "../Hamburger/Hamburger"
+import OverlayMenu from "../OverlayMenu/OverlayMenu"
+import Footer from "../Footer/Footer"
 import { GlobalStyles, Primary } from "./Layout.styles"
 
-
+//children comes from react, just renders stuff that 
 const Layout = ({ children }) => {
     const [menuOpen, setMenuOpen] = useState(false);
     
@@ -15,8 +17,10 @@ const Layout = ({ children }) => {
         <>
             <GlobalStyles />
             <Hamburger handleOverlayMenu={ handleOverlayMenu } />
+            <OverlayMenu menuOpen={ menuOpen } callback={ handleOverlayMenu } />
             <Header />
-            <Primary>{children}</Primary>
+            <Primary>{ children }</Primary>
+            <Footer />
         </>
     )
 }
