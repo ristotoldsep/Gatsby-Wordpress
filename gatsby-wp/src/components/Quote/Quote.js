e7a1932c980d19b7a27eb889974633c8735015a4
Risto Tõldsep <r.toldsep@gmail.com>
2022-09-23 17:34:24 +0300

    Added Quote and about components and queries

---
+++ gatsby-wp/src/components/Quote/Quote.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { useQuoteQuery } from "../../hooks/useQuoteQuery"
+import QuoteImg from "../../images/quote.svg"
+import { Wrapper, Content } from "./Quote.styles"
+
+const Quote = () => {
+    const data = useQuoteQuery()
+    console.log(data);
+
+    return (
+        <Wrapper>
+            <Content>
+                <img src={ QuoteImg } alt="Quote" />
+                <h6>{ data.wpPage.ACF_HomePage.citat1Text }</h6>
+            </Content>
+        </Wrapper>
+    )
+}
+
+export default Quote
--- gatsby-wp/src/pages/index.js
+++ gatsby-wp/src/pages/index.js
@@ -4,6 +4,8 @@ import Layout from "../components/Layout/Layout"
 import Hero from "../components/Hero/Hero"
 import CTAArea from "../components/CTAArea/CTAArea"
 import LatestBlogPost from "../components/LatestBlogPost/LatestBlogPost"
+import Quote from "../components/Quote/Quote"
+import About from "../components/About/About"
 
 
 const IndexPage = () => (
@@ -12,6 +14,8 @@ const IndexPage = () => (
       <Hero />
       <CTAArea />
       <LatestBlogPost />
+      <Quote />
+      <About />
     </Layout>
       
 
