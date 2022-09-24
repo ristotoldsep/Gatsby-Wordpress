75154fa0da9a9c05587da64b7240c48e91ac9854
Risto Tõldsep <r.toldsep@gmail.com>
2022-09-24 23:43:36 +0300

    Added page hero

---
+++ gatsby-wp/src/components/PageHero/PageHero.js
@@ -0,0 +1,19 @@
+import React from "react"
+import {
+  StyledImg,
+  Wrapper,
+  HeroWrapper
+} from "./PageHero.styles"
+
+const PageHero = ({ img }) => (
+  <>
+    <StyledImg image={img} alt="Page Hero" />
+    <Wrapper>
+      <HeroWrapper>
+            <p>Uauuua</p>
+        </HeroWrapper>
+    </Wrapper>
+  </>
+)
+
+export default PageHero
--- gatsby-wp/src/components/PageHero/PageHero.styles.js
+++ gatsby-wp/src/components/PageHero/PageHero.styles.js
@@ -4,4 +4,33 @@ import { GatsbyImage } from 'gatsby-plugin-image';
 export const StyledImg = styled(GatsbyImage)`
   height: 300px;
   width: 100%;
+  
 `;
+
+export const Wrapper = styled.div`
+  position: relative;
+  
+`
+
+export const HeroWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+
+  p {
+    font-size: 2rem;
+    font-weight: 600;
+    color: #fff;
+    text-transform: none;
+    text-align: center;
+
+    @media screen and (min-width: 768px) {
+      font-size: 4rem;
+    }
+  }
+`
